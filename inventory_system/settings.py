8fb06ae6aa30a5e45ee4f35df710025e193ce874
Ravi Kanani <ravikanani2003@gmail.com>
2025-03-07 21:58:22 +0530

    new changes

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o)@10$kugbt^l&#j&+u2oz_=dvp+*gru+zoks4&qry3ofuubl4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False   
+DEBUG = True   
 
-ALLOWED_HOSTS = ['rk2202.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 
